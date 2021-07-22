3f90511f1e193465bc55de99b725f2611d0cb9d3
nazila bagheri <nazila.bagheri93@gmail.com>
2021-07-22 17:10:58 +0430

    start project view pages

---
+++ ArtaTiam/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtaTiam.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cooperation()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}

39eaaa12df715078bc048cd9576415beec269982
nazila bagheri <nazila.bagheri93@gmail.com>
2021-08-21 17:33:05 +0430

    front edit

---
+++ ArtaTiam/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtaTiam.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+        public IActionResult EnProducts()
+        {
+            return View();
+        }
+    }
+}

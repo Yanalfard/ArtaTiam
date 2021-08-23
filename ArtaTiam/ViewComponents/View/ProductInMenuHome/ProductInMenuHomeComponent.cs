e39edb13ed966e9fbb6add24b91b1e85a30d4434
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-08-23 14:55:09 +0430

    update menu

---
+++ ArtaTiam/ViewComponents/View/ProductInMenuHome/ProductInMenuHomeComponent.cs
@@ -0,0 +1,22 @@
+﻿using ArtaTiam.Utilities;
+using DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ArtaTiam.ViewComponents.View.ProductInMenuHome
+{
+    public class ProductInMenuHomeComponent : ViewComponent
+    {
+        private Core _core = new Core();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("~/Views/Shared/Components/ProductInMenuHomeComponent" +
+                "/ProductInMenuHomeComponent.cshtml", _core.Catagory.Get(i => i.Parent == null)));
+        }
+    }
+}
